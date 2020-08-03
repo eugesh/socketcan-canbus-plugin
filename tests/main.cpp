f1b0f15f4b82f89aca7194f78e1e131d2656bbad
Evgeny Shtanov <shtanov_evgenii@mail.ru>
2020-08-04 01:22:30 +0300

    Delete unused code.
    Signed-off-by: Evgeny Shtanov <shtanov_evgenii@mail.ru>

--- tests/main.cpp
+++ tests/main.cpp
@@ -20,8 +20,6 @@ public slots:
     // void conf();
     // Write long UniCAN message.
     void longUniCANWrite();
-    // Write many messages without time gap.
-    void floodTrafficWrite();
 
 private:
     QCanBus *bus = nullptr;
@@ -68,68 +66,6 @@ void tst_QCanBus::initTestCase()
     }
 }
 
-void tst_QCanBus::floodTrafficWrite() {
-    for (int i = 0; i < 50000; ++i) {
-        const uint frameId = 0x22;
-        QString data = QString("E0FF");
-        const QByteArray payload = QByteArray::fromHex(data.toLatin1());
-
-        qInfo() << frameId << "#" << data;
-
-        QCanBusFrame frame = QCanBusFrame(frameId, payload);
-        // frame.setExtendedFrameFormat(false);
-        // frame.setFlexibleDataRateFormat(false);
-        // frame.setBitrateSwitch(m_ui->bitrateSwitchBox->isChecked());
-
-        /*if (m_ui->errorFrame->isChecked())
-            frame.setFrameType(QCanBusFrame::ErrorFrame);
-        else if (m_ui->remoteFrame->isChecked())
-            frame.setFrameType(QCanBusFrame::RemoteRequestFrame);*/
-
-        QVERIFY(m_canDevice->writeFrame(frame));
-    }
-}
-
-/*void tst_QCanBus::longUniCANWrite() {
-    // Name of file with long message
-    QString test_file = "long_CAN.txt";
-
-    for (int k = 0; k < LONG_MSG_TEST_NUM_LOOP; k++) {
-        QFile file(test_file);
-
-        if (!file.open(QFile::ReadOnly)) {
-            QString msg = tr("Failed to open %1\n%2")
-                            .arg(test_file)
-                            .arg(file.errorString());
-            qWarning() << tr("Error") + msg;
-            return;
-        }
-
-        while (!file.atEnd()) {
-            QByteArray line = file.readLine().simplified();
-
-            auto fields = line.split(' ');
-
-            int canId = fields[1].toUInt(nullptr, 16);
-            QByteArray len_arr = fields[2];
-            len_arr.replace('[', "");
-            len_arr.replace(']', "");
-            int len = len_arr.toUInt();
-            QByteArray payload;
-
-            for (int i = 0; i < len; ++i)
-                payload.append(QByteArray::fromHex(fields[3 + i]));
-
-            // qDebug() << canId << " [" << len << "] " << payload;
-
-            QCanBusFrame frame = QCanBusFrame(canId, payload);
-
-            QVERIFY(m_canDevice->writeFrame(frame));
-            // usleep(1);
-        }
-    }
-}*/
-
 void tst_QCanBus::longUniCANWrite() {
     // Name of file with long message
     QString test_file = "long_CAN.txt";
@@ -151,7 +87,6 @@ int main(int argc, char *argv[])
     tst_QCanBus test;
 
     test.initTestCase();
-    // test.floodTrafficWrite();
     test.longUniCANWrite();
 
     return a.exec();
