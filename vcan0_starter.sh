dd676471f5aa4020de79d166cc30a7e38d56ed06
Evgeny Shtanov <shtanov_evgenii@mail.ru>
2021-05-08 02:09:47 +0300

    vcan0_starter.sh fix: allows usage of CAN FD
    Signed-off-by: Evgeny Shtanov <shtanov_evgenii@mail.ru>

--- vcan0_starter.sh
+++ vcan0_starter.sh
@@ -1,6 +1,7 @@
 sudo modprobe vcan echo=1
 sudo ip link add dev vcan0 type vcan
-sudo ip link set vcan0 up txqueuelen 100
-sudo ip link set vcan0 up mtu 72
-sudo tc qdisc add dev vcan0 root tbf rate 1mbit burst 24b limit 10000
+sudo ip link set vcan0 txqueuelen 1000
+sudo ip link set vcan0 mtu 72
+sudo ip link set up vcan0
+sudo tc qdisc add dev vcan0 root tbf rate 1mbit burst 240b limit 10000
 
